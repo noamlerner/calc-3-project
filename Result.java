3aa6bf7dacb0b128e7b1f308eb2de18910471e3e
Michael Eden <themichaeleden@gmail.com>
2016-04-11 17:09:10 -0400

    added a Result monad for multiple return

---
+++ Result.java
@@ -0,0 +1,35 @@
+public class Result<V, E> {
+    private E failure;
+    private V success;
+
+    private Result(V success, E failure) {
+        this.success = success;
+        this.failure = failure;
+    }
+
+    public static <G, B> Result<G, B> fail(B failure) {
+        return new Result(null, failure);
+    }
+
+    public static <G, B> Result<G, B> succeed(G success) {
+        return new Result(success, null);
+    }
+
+    public boolean is_ok() {
+        return this.success != null;
+    }
+
+    public boolean is_err() {
+        return this.failure != null;
+    }
+
+    public V unwrap() {
+        assert this.is_ok();
+        return this.success;
+    }
+
+    public E unwrap_err() {
+        assert this.is_err();
+        return this.failure;
+    }
+}
