ae08db1c1dc380175abfdfffaacd550ddcca30a3
Michael Eden <themichaeleden@gmail.com>
2016-04-11 20:46:01 -0400

    initial power method working

--- PowerMethod.java
+++ PowerMethod.java
@@ -1,15 +1,15 @@
 public class PowerMethod {
-
-
-    public enum PowerMethodErr {
+    public static enum PowerMethodErr {
         MatrixNotSquare,
+        InvalidGuestVecDim,
+        InvalidAuxVecDim,
     }
 
-    public class EigenEstimate {
-        public final float value;
+    public static class EigenEstimate {
+        public final double value;
         public final Matrix vector;
 
-        public EigenEstimate(float value, Matrix vector) {
+        public EigenEstimate(double value, Matrix vector) {
             this.value = value;
             this.vector = vector;
         }
@@ -18,20 +18,29 @@ public class PowerMethod {
     public static Result<EigenEstimate, PowerMethodErr> power_method(
         Matrix matrix,
         Vector guess,
-        Vector auxiliary,
+        Vector aux,
         double tolerance,
         int iterations)
-    {
+
         // Check if its square
         if (matrix.getNumRows() != matrix.getNumCols()) {
             return Result.fail(PowerMethodErr.MatrixNotSquare);
+        } else if (matrix.getNumRows() != guess.length) {
+            return Result.fail(PowerMethodErr.InvalidGuestVecDim);
+        } else if (matrix.getNumRows() != guess.length) {
+            return Result.fail(PowerMethodErr.InvalidAuxVecDim);
         }
-        // Start power method
+        // Book-Keeping
         double error = Double.POSITIVE_INFINITY;
-        auxiliary = guess;
+        double eigen = Double.POSITIVE_INFINITY;
+        // Start power method
         for (int i = 0; i < iterations && error > tolerance; i += 1) {
-            auxiliary = matrix.times(auxiliary).norm();
+            Vector next_vec = matrix.times(guess);
+            double next_val = next_vec.dot(aux) / guess.dot(aux);
+            error = Math.abs(eigen - next_val);
+            guess = next_vec;
+            eigen = next_val;
         }
-        return null;
+        return Result.succeed(new EigenEstimate(eigen, guess.norm()));
     }
 }
