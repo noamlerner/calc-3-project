83d3d71be0a67101cc26f0f33ee726a743366e04
noamlerner <noaml1295@gmail.com>
2016-04-19 16:58:42 -0400

    'tests'

---
+++ src/main/tests/InverseTest.java
@@ -0,0 +1,25 @@
+package main.tests;
+
+import main.Matrix;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class InverseTest {
+	@Test
+	public void testInverse(){
+		for(int i = 2; i < 7; i++){
+			for(int j = 0; j < 10000; j++){
+				inverseOfSize(i);
+			}
+		}
+	}
+
+	public void inverseOfSize(int size){
+		Matrix A = Matrix.random(size, size);
+		Matrix invA = A.inverse();
+		Matrix I = Matrix.identity(size);
+		assertEquals(A.times(invA), I);
+		assertEquals(invA.times(A), I);
+	}
+}
